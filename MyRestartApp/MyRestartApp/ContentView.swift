3b56662753516a1cfda438ecf64e0c76a50eede3
최민한 <chmh0411@gmail.com>
2022-05-22 21:09:10 +0900

    create: MyRestartApp

---
+++ MyRestartApp/MyRestartApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyRestartApp
+//
+//  Created by 최민한 on 2022/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyRestartApp/MyRestartApp/MyRestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRestartAppApp.swift
+//  MyRestartApp
+//
+//  Created by 최민한 on 2022/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MyRestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
